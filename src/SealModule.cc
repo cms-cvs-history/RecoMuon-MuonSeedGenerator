ebcfda1cb75bfaaf5ce31fd60beef15255c2c19d
Riccardo Bellan <sha1-23462e7af5d1c5b2463bad896e1b93452126c1e5@cern.ch>
2006-04-26 07:06:05 +0000

    Producer moved from interface

---
+++ src/SealModule.cc
@@ -0,0 +1,9 @@
+#include "PluginManager/ModuleDef.h"
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "RecoMuon/MuonSeedGenerator/src/MuonSeedGenerator.h"
+
+DEFINE_SEAL_MODULE();
+DEFINE_ANOTHER_FWK_MODULE(MuonSeedGenerator)
+
